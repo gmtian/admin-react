b8f1c963ef815a5145cb99ce3fe0602b27816739
葛梦甜 <1711285659@qq.com>
2019-04-29 14:19:16 +0800

    简单路由切换

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter, hashRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { Layout } from 'antd';
+import Sider from './common/components/Sider';
+import Header from './common/components/Header';
+import BookPage from './pages/Book';
+import UserPage from './pages/User';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <p>App</p>
-      </div>
+      <BrowserRouter>
+        <Layout>
+          <Sider />
+          <Layout style={{ marginLeft: 200 }}>
+            <Header />
+            <Layout.Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+              <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                <Switch>
+                  <Route path='/' exact component={BookPage}></Route>
+                  <Route path='/user' exact component={UserPage}></Route>
+                  <Redirect to="/"></Redirect>
+                </Switch>
+              </div>
+            </Layout.Content>
+          </Layout>
+        </Layout>
+      </BrowserRouter>
     )
   }
 }
 
-export default App;
+export default App
---
+++ src/pages/Book/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Book extends Component {
+  render() {
+    return (
+      <div>
+        图书管理
+      </div>
+    )
+  }
+}
+export default Book;
